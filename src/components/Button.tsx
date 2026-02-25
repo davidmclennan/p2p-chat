4d1182a13ad6a4aa659c48fb4e80828ba23d44c6
David McLennan <dmclennan92@gmail.com>
2026-02-25 17:18:41 +0200

    Add tailwind theme setup

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import { ParentComponent } from "solid-js";
 
 const Button: ParentComponent = (props) => {
   return (
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button class="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded">
       {props.children}
     </button>
   );
